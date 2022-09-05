c12271da4a7f4b19dc6134559a91779f41948854
Franklin <Franklinau2@gmail.com>
2022-09-05 15:45:05 -0700

    Add inorder method

--- bst.rb
+++ bst.rb
@@ -110,6 +110,21 @@ class Tree
     end
   end
 
+  def inorder(root = @root, &block)
+    return if root.nil?
+    if block_given?
+      inorder(root.left_child, &block)
+      yield(root)
+      inorder(root.right_child, &block)
+    else
+      arr = []
+      arr.push(inorder(root.left_child)).flatten!
+      arr.push(root.data)
+      arr.push(inorder(root.right_child)).flatten!
+      arr.compact
+    end
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -135,4 +150,4 @@ arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 # #arr = [1, 2, 3, 4, 5, 6, 7, 8, 10]
 tree = Tree.new(arr)
 tree.pretty_print
-p tree.preorder
+p tree.inorder #{ |node| puts node.data }
